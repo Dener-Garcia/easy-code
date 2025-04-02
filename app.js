60719f3e5fdf1f1dc34a6a8a08122a36a98aee2c
Dener Garcia <dener.criarbr@gmail.com>
2025-04-02 13:19:41 -0300

    feat: create pwa

--- app.js
+++ app.js
@@ -32,3 +32,13 @@ if (isValidHTML(codeArea.value)) {
 })
 
 
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker.register("/sw.js").then(() => {
+        console.log("Service Worker registrado!");
+      }).catch((err) => {
+        console.log("Erro ao registrar o Service Worker", err);
+      });
+    });
+  }
+  
